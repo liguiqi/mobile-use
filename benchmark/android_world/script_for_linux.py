6810f27f7e1d60924545f65526d5fe58a63f1350
Ning Li <2212363918@qq.com>
2025-04-30 16:26:32 +0800

    feat: Add script for AndroidWorld

--- benchmark/android_world/script_for_linux.py
+++ benchmark/android_world/script_for_linux.py
@@ -7,11 +7,13 @@ parser = argparse.ArgumentParser(description="Run Android emulator and execute s
 parser.add_argument("--checkpoint_dir", type=str, default="~/android_world/runs/ckpt", help="Path to the checkpoint directory.")
 parser.add_argument("--output_path", type=str, default="./output.log", help="Path to the output file.")
 parser.add_argument("--max_step", type=str, default="15", help="Path to the output file.")
+parser.add_argument("--n_task_combinations", type=int, default=5, help="Name of the emulator to run.")
 
 args = parser.parse_args()
 ckpt_dir = args.checkpoint_dir
 output_path = args.output_path
 max_step = args.max_step
+n_task_combinations = args.n_task_combinations
 
 def kill_emulator():
     command = 'pkill -f "qemu-system-x86_64"'
@@ -26,7 +28,7 @@ for _ in range(100):
     print("Emulator started")
     time.sleep(120)
     print("Running script")
-    subprocess.run(f"python -u run.py --checkpoint_dir={ckpt_dir} --mobileuse_agent_name=MultiAgent >> logs\name.log 2>&1", shell=True)
+    subprocess.run(f"python -u run.py --checkpoint_dir={ckpt_dir} --n_task_combinations={n_task_combinations} >> {output_path} 2>&1", shell=True)
     print("Script ran")
     print("Force Killing emulator")
     kill_emulator()
